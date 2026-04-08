8419c6f03eaa527700d9e853d4d9c005c4d0c07e
Sneha Patel <snehapatel@cybage.com>
2026-04-08 13:25:41 +0530

    Reverse string and largest number

---
+++ src/NumberPrograms/FindLargest.java
@@ -0,0 +1,20 @@
+package NumberPrograms;
+
+public class FindLargest {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {23,65,12,3,1,98,45};
+		int largest = arr[0];
+		
+		//tip: You don’t need to start loop from 0 again since you already took arr[0].
+		for(int i = 1; i<arr.length; i++) {
+			if(largest<arr[i])
+				largest = arr[i];
+		}
+		System.out.println(largest);
+	}
+
+}
+
+//Output: 98
---
+++ src/Stringprograms/ReverseString.java
@@ -0,0 +1,32 @@
+package Stringprograms;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		
+		//Using StringBuilder
+		String str = "Sneha";
+		StringBuilder rev= new StringBuilder();
+		
+		
+		for(int i = str.length()-1; i>=0; i--) {
+			rev.append(str.charAt(i));
+		}
+		
+		System.out.println(rev);
+
+		//Without using StringBuilder
+//		String str = "Sneha";
+//		String rev = "";
+//		
+//		for(int i = str.length()-1; i>=0; i--) {
+//			rev = rev + str.charAt(i);
+//		}
+//		System.out.println(rev);
+	}
+
+}
+
+//Output: ahenS
+
